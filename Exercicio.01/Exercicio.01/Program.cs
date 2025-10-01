3e053960b127e5bd56caefe33a17b8856f449536
Fernanda Locatelli <locatelifernanda3@gmail.com>
2025-10-01 08:54:32 -0300

    MEDIA DAS NOTAS DO ALUNO

---
+++ Exercicio.01/Exercicio.01/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int quantidade = 0;
+Console.Write("Digite a quantidade de notas que deseja incluir: ");
+quantidade = int.Parse(Console.ReadLine());
+
+double[] notas = new double[quantidade];
+double soma = 0;
+
+for (int i = 0; i < quantidade; i++)
+{
+    Console.Write($"Digite a {i + 1}ª nota: ");
+    notas[i] = double.Parse(Console.ReadLine());
+    soma += notas[i]; // LEMBRAR DE USAR QUANDO PRECISAR ACUMULAR VALORES
+}
+
+double media = soma / quantidade;
+
+// Resultado
+Console.WriteLine("******************************** ");
+Console.WriteLine($"A média do aluno é {media:F2}. ");
+Console.WriteLine("******************************** ");
