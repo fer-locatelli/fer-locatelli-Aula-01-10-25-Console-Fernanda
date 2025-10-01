a81616e274b9df6560a6634482d9296366634193
Fernanda Locatelli <locatelifernanda3@gmail.com>
2025-10-01 08:30:33 -0300

    ATIVIDADE EXEMPLO FRACALOSSI  / DIEGO

---
+++ Exemplo prático/Exemplo prático/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// EXEMPLO QUADRO - FRACALOSSI //
+
+int quantidade = 0;
+Console.WriteLine("Digite a quantidade de pessoas que deseja incluir: ");
+quantidade = int.Parse(Console.ReadLine());
+string[] nomes = new string[quantidade];
+
+for(int i=0; i < nomes.Length; i++)
+{
+    Console.WriteLine($"Digite o {i + 1}° Nome: ");
+    nomes[i] = Console.ReadLine();
+}
+
+Console.WriteLine("Nomes incluidos no vetor NOMES[] ");
+Console.WriteLine("******************************** ");
+
+for (int i=0; i < nomes.Length; i++)
+{
+    Console.WriteLine($"Posição [{1}] = {nomes[i]} ");
+}
+
+Console.WriteLine("******************************** ");
