ea28d51c979eb48b2da8f0a250cf71b60790f068
Fernanda Locatelli <locatelifernanda3@gmail.com>
2025-10-21 08:24:43 -0300

    Prova 5º questão

---
+++ PROVA.05.QUESTAO/PROVA.05.QUESTAO/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// PROVA 5º QUESTÃO//
+
+int a = 17;
+int b = 3;
+int c = 1;
+
+c += a / b;
+
+a *= c;
+b -= a % 4;
+
+Console.WriteLine($"a={a}, b={b}, c={c}");
